5c921b752bd88ba19a4e7137d369708a3d5efd9d
vikasna <vikas.nv@gmail.com>
2025-03-24 13:20:01 +0530

    force lightmode always on Windows
    #if defined(Q_OS_WIN)
    	qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
    #endif

--- src/sdk/main.cpp
+++ src/sdk/main.cpp
@@ -103,6 +103,9 @@ static void cleanupUpdate(const CommandLineParser &parser, bool *exit)
 
 int main(int argc, char *argv[])
 {
+#if defined(Q_OS_WIN)
+	qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
+#endif
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     if (!qEnvironmentVariableIsSet("QT_AUTO_SCREEN_SCALE_FACTOR")
             && !qEnvironmentVariableIsSet("QT_SCALE_FACTOR")
